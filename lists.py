07f4a81536e111c5b664d197a9c5268cda95fb81
Pavan19047 <118459281+Pavan19047@users.noreply.github.com>
2023-07-11 13:19:06 +0530

    Delete lists.py

--- lists.py
+++
@@ -1,6 +0,0 @@
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
- 
-dirty_dozen = [fruits, vegetables]
- 
-print(dirty_dozen[1][1])
